498feffaee8b290c47e0e4a24f79d75349cfec81
yunfei.wang <1669136244@qq.com>
2023-09-04 14:05:53 +0800

    init

---
+++ src/component/Side.vue
@@ -0,0 +1,14 @@
+<!--
+ * @Descripttion: 
+ * @Author: yunfei
+ * @Date: 2023-09-04 13:57:54
+-->
+<template>
+  <div>side</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
